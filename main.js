866d8b170a1e5910641b7c981dc7f871bb615589
Jorge Gamboa <j.gamboafernandez@gmail.com>
2026-08-18 15:51:56 -0400

    update: ingresar btn functionality added, show and button X working

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 const login_btn = document.querySelector("#login_btn");
 const popup_container_display_none = document.querySelector(".popup_container_display_none");
-
-
+const popupCloseBtn = document.querySelector("#popup_close_btn");
+ 
 
 
 login_btn.addEventListener("click", () => {
@@ -12,6 +12,10 @@ login_btn.addEventListener("click", () => {
     popup_container_display_none.style.transition = "600ms all ease-in-out"; 
 })
 
+popupCloseBtn.addEventListener("click", () => {
+    popup_container_display_none.style.display = "none"
+    
+})
 
 
 
