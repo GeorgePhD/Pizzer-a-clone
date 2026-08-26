482c53791fddea6ae22953f8941468eaf87aa9e5
Jorge Gamboa <j.gamboafernandez@gmail.com>
2026-08-26 19:34:16 -0400

    minor changes

--- main.js
+++ main.js
@@ -5,19 +5,6 @@ const popup_container_display_none = document.querySelector(".popup_container_di
 const popupCloseBtn = document.querySelector("#popup_close_btn");
 const body = document.querySelector("body");
 const slider = document.querySelector(".slider");
-const slider_1 = document.querySelector("#slider_img_1");
-const slider_2 = document.querySelector("#slider_img_2")
-const slider_3 = document.querySelector("#slider_img_3")
-const slider_4 = document.querySelector("#slider_img_4")
-const slider_5 = document.querySelector("#slider_img_5")
-
-const sliders = [
-    slider_1,
-    slider_2,
-    slider_3,
-    slider_4,
-    slider_5
-]
 
 
 login_btn.addEventListener("click", () => {
@@ -46,14 +33,5 @@ setInterval(() => {
     slider.style.transform = `translateX(-${currentSlider * 20}%)`;
 
 }, 3000);
-
-
-
-
-
-
-
-
-
 
 
--- plan.md
+++ plan.md
@@ -32,8 +32,12 @@ date : 19082026 (undone, Maty was busy, content moved to next session)
 3. push information
 
 date : 21082026 (https://extract.pics/)
-0. install opencode (https://opencode.ai/docs/es)
+0. install opencode (https://opencode.ai/docs/es) ???? it is a fine idea?
 1. add close button (X) to popup
-2. check 2 menus that are ready.
+2. check 2 menus that are ready
 3. create slider
 4. push information
+
+date : 26082026
+1. slider
+2. push information to github
